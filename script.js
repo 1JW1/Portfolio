2e531d8b222f7c8ae156f787c64d63d588ff1af9
Jerome Wanliss <jerome@Jeromes-MacBook-Pro.local>
2020-08-12 21:45:36 +0100

    updated look and content

--- script.js
+++ script.js
@@ -21,11 +21,11 @@ for (var i=0; themeDots.length > i; i++){
 
 function setTheme(mode){
 	if(mode == 'light'){
-		document.getElementById('theme-style').href = 'default.css'
+		document.getElementById('theme-style').href = 'light.css'
 	}
 
 	if(mode == 'blue'){
-		document.getElementById('theme-style').href = 'blue.css'
+		document.getElementById('theme-style').href = 'default.css'
 	}
 
 	if(mode == 'green'){
